7342a14ca0b27f4788b8dc4c201cd0545501c85e
AidenH <aidenalexh@gmail.com>
2022-02-15 17:06:01 -0500

    update tests with "tinkering" mode for no return

--- market-interface_test.go
+++ market-interface_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/sacOO7/gowebsocket"
 )
 
+var tinkering = true
+
 func TestSocketInit(t *testing.T) {
 	if err := SocketInit(); err != nil {
 		fmt.Println(err.Error())
@@ -46,7 +48,8 @@ func TestTradesSocket(t *testing.T) {
 	socket.Connect()
 
 	for resp == "" {
-		if tr.Type == "subscribed" {
+		// pass on successful subscribe
+		if tr.Type == "subscribed" && !tinkering {
 			return
 		}
 	}
@@ -80,14 +83,18 @@ func TestOrdersSocket(t *testing.T) {
 	}
 
 	socket.OnTextMessage = func(msg string, socket gowebsocket.Socket) {
-		json.Unmarshal([]byte(msg), &ord)
+		if err := json.Unmarshal([]byte(msg), &ord); err != nil {
+			fmt.Println(err.Error())
+			t.Fail()
+		}
 		fmt.Println(ord)
 	}
 
 	socket.Connect()
 
 	for resp == "" {
-		if ord.Type == "subscribed" {
+		// pass on successful subscribe
+		if ord.Type == "subscribed" && !tinkering {
 			return
 		}
 	}
