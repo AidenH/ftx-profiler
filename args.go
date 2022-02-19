e29fc3c06389c7e12662588c9dd837af66fd2e86
AidenH <aidenalexh@gmail.com>
2022-02-19 18:40:43 -0500

    long and short form args

--- args.go
+++ args.go
@@ -11,15 +11,15 @@ func HandleOsArgs() {
 
 	for _, item := range args {
 
-		if item == "tape-only" {
+		if item == "-t" || item == "--tape-only" {
 			State.ProfileTrue = false
 			log.Println("tape-only enabled")
 
-		} else if item == "profile-only" {
+		} else if item == "-p" || item == "--profile-only" {
 			State.TapeTrue = false
 			log.Println("profile-only enabled")
 
-		} else if item == "volume-counts" {
+		} else if item == "-v" || item == "--volume-counts" {
 			State.VolumeCounts = true
 			log.Println("starting with profile volume counts enabled")
 
